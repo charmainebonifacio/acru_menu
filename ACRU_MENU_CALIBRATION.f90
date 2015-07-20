d173ee43ffd7abf725396ff70e462c171272330c
charmainebonifacio <charmaine.bonifacio@gmail.com>
2015-07-20 16:12:09 -0600

    Update Menu Calibration script
    Script now processes Albedo, Cay, Elaim and Roota variables. There is no
    inserting of additional ISUBNO lines but replaces the actual values.
    Previous version only replaced Albedo values correctly. Cay variables
    were inserted at the appropriate line, which meant losing part of the
    SNEREL variables.

--- ACRU_MENU_CALIBRATION.f90
+++ ACRU_MENU_CALIBRATION.f90
@@ -4,11 +4,10 @@
 ! CREATED BY   : Dr. Stefan W. Kienzle
 ! DATE EDITED  : May 19, 2008
 ! REVISED BY   : Charmaine Bonifacio
-! DATE REVISED : July 14, 2015
+! DATE REVISED : July 20, 2015
 !-------------------------------------------------------------------
 ! DESCRIPTION  : The program will copy values from a tab delimited
-!                file that contains COIAM, CAY, ELAIM, ROOTA, ICC,
-!                Albedo.
+!                file that contains ALBEDO, CAY, ELAIM, ROOTA.
 ! REQUIREMENT  : MUST run the .EXE file within the input directory.
 ! INPUT        : 1) MENU FILE = MENU
 !                2) VARIABLE FILE = menu_variable.txt
@@ -28,8 +27,8 @@ REAL :: D1, D2
 REAL, DIMENSION(12) :: COIAM, CAY, ELAIM, ROOTA, ICC, ALBEDO
 CHARACTER(LEN=4), PARAMETER :: MENU = 'MENU'
 CHARACTER(LEN=50), PARAMETER :: MENUVARS = 'menu_variable.txt'
-CHARACTER(LEN=200) :: OUTFILE, INFILE, LOGRUN, VARFILE
-CHARACTER(LEN=129) :: DUM, DUM2
+CHARACTER(LEN=100) :: OUTFILE, INFILE, LOGRUN, VARFILE
+CHARACTER(LEN=80) :: DUM, DUM2
 CHARACTER(LEN=8) :: DATEINFO
 CHARACTER(LEN=4) :: YEAR, MONTH*2, DAY*2
 CHARACTER(LEN=2) :: HRS, MIN, SEC*6
@@ -62,7 +61,7 @@ LOGICAL :: EX
 !***********************************************************************
 ! DATE
 !***********************************************************************
-	    WRITE(*,*) debugSTAT, ' Start Date of log  -> ', DATENOW
+	  WRITE(*,*) debugSTAT, ' Start Date of log  -> ', DATENOW
       WRITE(*,*) debugSTAT, ' Start Time of log  -> ', TIMENOW
       WRITE(*,*)
       LOGRUN = 'LOGRUN_MENU_'//DATE//'.txt'
@@ -77,8 +76,8 @@ LOGICAL :: EX
         WRITE(*,*) debugRES, 'COULD NOT OPEN FILE.'
         STOP
       ENDIF
-	    WRITE(*,*) debugRES, ' File opened: ', LOGRUN
-	    WRITE(*,*) debugRES, ' File status ok = ', OK
+	  WRITE(*,*) debugRES, ' File opened: ', LOGRUN
+	  WRITE(*,*) debugRES, ' File status ok = ', OK
       WRITE(12,*)
 !***********************************************************************
 ! START LOG
@@ -91,7 +90,7 @@ LOGICAL :: EX
       WRITE(12,*) ' '
       WRITE(12,*) "###################################################################"
       WRITE(12,*)
-	    WRITE(12,*) debugSTAT, ' DATE -> ', DATENOW
+	  WRITE(12,*) debugSTAT, ' DATE -> ', DATENOW
       WRITE(12,*) debugSTAT, ' TIME -> ', TIMENOW
       WRITE(12,*)
       WRITE(12,*) debugSTAT, ' LOGFILE -> ', LOGRUN
@@ -110,19 +109,19 @@ LOGICAL :: EX
       WRITE(12,*)
       WRITE(12,*) debugSTAT, ' Process files..... '
       VARFILE = MENUVARS
-	    OPEN(UNIT=11,FILE=VARFILE,IOSTAT=OK)
-	    WRITE(*,*) debugRES, ' File opened: ', VARFILE
-	    WRITE(*,*) debugSTAT, ' File status ok = ', OK
+	  OPEN(UNIT=11,FILE=VARFILE,IOSTAT=OK)
+	  WRITE(*,*) debugRES, ' File opened: ', VARFILE
+	  WRITE(*,*) debugSTAT, ' File status ok = ', OK
       WRITE(12,*) debugRES, ' VARIABLE FILE -> ', VARFILE
       WRITE(12,*) debugSTAT, ' STATUS -> ', OK
       OPEN(UNIT=20,FILE=OUTFILE,IOSTAT=OK)
       WRITE(*,*) debugRES, ' File opened: ', OUTFILE
-	    WRITE(*,*) debugSTAT, ' File status ok = ', OK
+	  WRITE(*,*) debugSTAT, ' File status ok = ', OK
       WRITE(12,*) debugRES, ' MENUFILE COPY -> ', OUTFILE
       WRITE(12,*) debugSTAT, ' STATUS -> ', OK
       OPEN(UNIT=30,FILE=INFILE,IOSTAT=OK)
       WRITE(*,*) debugRES, ' File opened: ', INFILE
-	    WRITE(*,*) debugSTAT, ' File status ok = ', OK
+	  WRITE(*,*) debugSTAT, ' File status ok = ', OK
       WRITE(12,*) debugRES, ' MENUFILE -> ', INFILE
       WRITE(12,*) debugSTAT, ' STATUS -> ', OK
 !***********************************************************************
@@ -134,7 +133,7 @@ LOGICAL :: EX
         READ(20,111) DUM2
         P=P+1
   898 CONTINUE
-  111 FORMAT(A129)
+  111 FORMAT(A80)
       READ(20,112) ISUBNO
       WRITE(12,112) ISUBNO
   112 FORMAT(3X,I4)
@@ -151,7 +150,7 @@ LOGICAL :: EX
       DO 899 WHILE (TOTALLINE.LT.LINEEOF)
         READ(20,111) DUM2
         TOTALLINE=TOTALLINE+1
-  899 CONTINUE
+  899 END DO
       CLOSE(20)
       WRITE(*,*) 'Number of lines read? ',TOTALLINE
       WRITE(*,*) 'Number of lines as per HRU calculation? ',LINEEOF
@@ -163,12 +162,12 @@ LOGICAL :: EX
 ! Calculate line number for each variable!
 ! Then overwrite values once line is found. Continue for X HRUs.
 !***********************************************************************
-	    LINEALBEDO=23+((ISUBNO+5)*28)
+	  LINEALBEDO=23+((ISUBNO+5)*28)
       LINECAY   =23+((ISUBNO+5)*53)
       LINEELAIM =23+((ISUBNO+5)*54)
-	    LINEROOTA =23+((ISUBNO+5)*56)
+	  LINEROOTA =23+((ISUBNO+5)*56)
       LINECOIAM =23+((ISUBNO+5)*67)
-	    LINEICC   =23+((ISUBNO+5)*141)
+	  LINEICC   =23+((ISUBNO+5)*141)
       WRITE(*,*) 'ALBEDO Line = ',LINEALBEDO
       WRITE(*,*) '   CAY Line = ',LINECAY
       WRITE(*,*) ' ELAIM Line = ',LINEELAIM
@@ -187,16 +186,17 @@ LOGICAL :: EX
       LINE=1
       DO 900 WHILE (LINE.LT.LINEEOF)
         L=1
-	      READ(20,111) DUM
 !=================================================
-! CHECK WHERE ALBEDO SHOULD BE OVERWRITTEN
-        IF(LINE.EQ.LINEALBEDO) THEN
+! CHECK WHERE VARIABLES SHOULD BE OVERWRITTEN
+        IF(LINE.EQ.LINEALBEDO) THEN ! CHECK WHERE ALBEDO SHOULD BE OVERWRITTEN  
           OPEN(UNIT=11,FILE=VARFILE)
-          WRITE(12,*) debugRES, ' File opened: ', VARFILE
-	        WRITE(12,*) debugSTAT, ' File status ok = ', OK
-          READ(11,111) DUM2  ! Header
-	        READ(11,111) DUM2  ! Header
+          READ(11,*) DUM2  ! Header
+	      READ(11,*) DUM2  ! Header
+          WRITE(12,*) '=================================================================================' 
+          WRITE(12,*) debugSTAT, ' .... ALBEDO CALIBRATION STARTING FROM LINE >> ', LINE         
           DO 901 WHILE (L.LE.ISUBNO)
+            WRITE(*,*) debugSTAT, ' Processing Line >> ',LINE
+            WRITE(12,*) debugSTAT, ' Processing Line >> ',LINE            
             READ(11,*)D1,D2, &
               (COIAM(I),I=1,12),(CAY(I),I=1,12), &
               (ELAIM(I),I=1,12),(ROOTA(I),I=1,12), &
@@ -205,46 +205,92 @@ LOGICAL :: EX
   220       FORMAT(66X,I1,5X,I1)
 	        WRITE(12,222)(ALBEDO(I),I=1,12),ICONS,ISWAVE,L
  	        WRITE(30,222)(ALBEDO(I),I=1,12),ICONS,ISWAVE,L
-  222     FORMAT(1X,11(F4.2,' '),F4.2,6X,I1,5X,I1,3X,I4)
-          L=L+1
+  222       FORMAT(1X,11(F4.2,' '),F4.2,6X,I1,5X,I1,3X,I4)
+            WRITE(*,*) debugRES, ' CALIBRATED LINE ',LINE, '--- HRU # ',L, ' OUT OF ',ISUBNO
+            WRITE(12,*) debugRES, ' CALIBRATED LINE ',LINE, '--- HRU # ',L, ' OUT OF ',ISUBNO
+            L=L+1
 	        LINE=LINE+1
-          WRITE(*,*) debugSTAT, ' Processing Variable Line ',LINE, ' --- Overwriting ALBEDO'
-          WRITE(12,*) debugSTAT, ' Processing Variable Line ',LINE, ' --- Overwriting ALBEDO'
-  901     CONTINUE
+  901     END DO
+          WRITE(12,*) '=================================================================================' 
           CLOSE(11)
-        ENDIF
-! END CHECK WHERE ALBEDO SHOULD BE OVERWRITTEN
-!=================================================
-! CHECK WHERE CAY SHOULD BE OVERWRITTEN
-        IF(LINE.EQ.LINECAY) THEN
+        ELSEIF(LINE.EQ.LINECAY) THEN ! CHECK WHERE CAY SHOULD BE OVERWRITTEN  
           OPEN(UNIT=11,FILE=VARFILE)
-          WRITE(12,*) debugRES, ' File opened: ', VARFILE
-	        WRITE(12,*) debugRES, ' File status ok = ', OK
-          READ(11,111) DUM2  ! Header
-	        READ(11,111) DUM2  ! Header
+          READ(11,*) DUM2  ! Header
+	      READ(11,*) DUM2  ! Header
+          WRITE(12,*) '=================================================================================' 
+          WRITE(12,*) debugSTAT, ' .... CAY CALIBRATION STARTING FROM LINE >> ', LINE   
           DO 902 WHILE (L.LE.ISUBNO)
+            WRITE(*,*) debugSTAT, ' Processing Line >> ',LINE
+            WRITE(12,*) debugSTAT, ' Processing Line >> ',LINE
             READ(11,*)D1,D2, &
               (COIAM(I),I=1,12),(CAY(I),I=1,12), &
               (ELAIM(I),I=1,12),(ROOTA(I),I=1,12), &
               (ICC(I),I=1,12),(ALBEDO(I),I=1,12)
-	          WRITE(12,221)(CAY(I),I=1,12),(L)
-	          WRITE(30,221)(CAY(I),I=1,12),(L)
+            READ(20,111) DUM
+	        WRITE(12,221)(CAY(I),I=1,12),(L)
+	        WRITE(30,221)(CAY(I),I=1,12),(L)
   221       FORMAT(1X,12(F4.2,','),15X,I4)
+            WRITE(*,*) debugRES, ' CALIBRATED LINE ',LINE, '--- HRU # ',L, ' OUT OF ',ISUBNO
+            WRITE(12,*) debugRES, ' CALIBRATED LINE ',LINE, '--- HRU # ',L, ' OUT OF ',ISUBNO
             L=L+1
 	        LINE=LINE+1
-          WRITE(*,*) 'Processing Variable Line ',LINE, ' --- Overwriting CAY'
-          WRITE(12,*) 'Processing Variable Line ',LINE, ' --- Overwriting CAY'
-  902     CONTINUE
+  902     END DO
+          WRITE(12,*) '=================================================================================' 
           CLOSE(11)
+        ELSEIF(LINE.EQ.LINEELAIM) THEN ! CHECK WHERE ELAIM SHOULD BE OVERWRITTEN  
+          OPEN(UNIT=11,FILE=VARFILE)
+          READ(11,*) DUM2  ! Header
+	      READ(11,*) DUM2  ! Header
+          WRITE(12,*) '=================================================================================' 
+          WRITE(12,*) debugSTAT, ' .... ELAIM CALIBRATION STARTING FROM LINE >> ', LINE   
+          DO 903 WHILE (L.LE.ISUBNO)
+            WRITE(*,*) debugSTAT, ' Processing Line >> ',LINE
+            WRITE(12,*) debugSTAT, ' Processing Line >> ',LINE
+            READ(11,*)D1,D2, &
+              (COIAM(I),I=1,12),(CAY(I),I=1,12), &
+              (ELAIM(I),I=1,12),(ROOTA(I),I=1,12), &
+              (ICC(I),I=1,12),(ALBEDO(I),I=1,12)
+            READ(20,111) DUM
+	        WRITE(12,221)(ELAIM(I),I=1,12),(L)
+	        WRITE(30,221)(ELAIM(I),I=1,12),(L)
+            WRITE(*,*) debugRES, ' CALIBRATED LINE ',LINE, '--- HRU # ',L, ' OUT OF ',ISUBNO
+            WRITE(12,*) debugRES, ' CALIBRATED LINE ',LINE, '--- HRU # ',L, ' OUT OF ',ISUBNO
+            L=L+1
+	        LINE=LINE+1
+  903     END DO
+          WRITE(12,*) '=================================================================================' 
+          CLOSE(11)
+        ELSEIF(LINE.EQ.LINEROOTA) THEN ! CHECK WHERE ELAIM SHOULD BE OVERWRITTEN  
+          OPEN(UNIT=11,FILE=VARFILE)
+          READ(11,*) DUM2  ! Header
+	      READ(11,*) DUM2  ! Header
+          WRITE(12,*) '=================================================================================' 
+          WRITE(12,*) debugSTAT, ' .... ROOTA CALIBRATION STARTING FROM LINE >> ', LINE   
+          DO 904 WHILE (L.LE.ISUBNO)
+            WRITE(*,*) debugSTAT, ' Processing Line >> ',LINE
+            WRITE(12,*) debugSTAT, ' Processing Line >> ',LINE
+            READ(11,*)D1,D2, &
+              (COIAM(I),I=1,12),(CAY(I),I=1,12), &
+              (ELAIM(I),I=1,12),(ROOTA(I),I=1,12), &
+              (ICC(I),I=1,12),(ALBEDO(I),I=1,12)
+            READ(20,111) DUM
+	        WRITE(12,221)(ROOTA(I),I=1,12),(L)
+	        WRITE(30,221)(ROOTA(I),I=1,12),(L)
+            WRITE(*,*) debugRES, ' CALIBRATED LINE ',LINE, '--- HRU # ',L, ' OUT OF ',ISUBNO
+            WRITE(12,*) debugRES, ' CALIBRATED LINE ',LINE, '--- HRU # ',L, ' OUT OF ',ISUBNO
+            L=L+1
+	        LINE=LINE+1
+  904     END DO
+          WRITE(12,*) '=================================================================================' 
+          CLOSE(11)
+        ELSE ! SIMPLY READ AND COPY LINES
+          READ(20,111) DUM
+          WRITE(30,111) DUM
+          WRITE(*,*) debugSTAT, ' PROCESSED LINE ',LINE
+          WRITE(12,*) debugSTAT, ' PROCESSED LINE ',LINE
+          LINE=LINE+1
         ENDIF
-! END CHECK WHERE CAY SHOULD BE OVERWRITTEN
-!=================================================
-! COPY MENU LINES
-        WRITE(30,111) DUM
-	      WRITE(*,*) debugSTAT, ' PROCESSING LINE ',LINE
-        WRITE(12,*) debugSTAT, ' PROCESSING LINE ',LINE
-        LINE=LINE+1
-  900 CONTINUE
+  900 END DO
       CLOSE(20)
       DUM2 = ' MENU SCRIPT VERSION JULY 2015 --- MENU CALIBRATED & CREATED BY CHARMAINE BONIFACIO '
       WRITE(30,111) DUM2
@@ -275,8 +321,7 @@ LOGICAL :: EX
       WRITE(*,*)
       WRITE(*,*) debugSTAT, ' End Date of log  -> ', DATEEND
       WRITE(*,*) debugSTAT, ' End Time of log  -> ', TIMEEND
-      WRITE(*,*)
-      WRITE(*,*) debugSTAT, ' Time Elapsed : ', REAL(COUNT_1 - COUNT_0)/ REAL(COUNT_RATE)
+      WRITE(*,*) debugSTAT, ' ELAPSED TIME : ', REAL(COUNT_1 - COUNT_0)/ REAL(COUNT_RATE)
       WRITE(*,*)
       WRITE(*,*) 'END OF PROGRAM. '
       WRITE(12,*) "###################################################################"
@@ -287,8 +332,7 @@ LOGICAL :: EX
       WRITE(12,*)
       WRITE(12,*) debugSTAT, ' End Date of log  -> ', DATEEND
       WRITE(12,*) debugSTAT, ' End Time of log  -> ', TIMEEND
-      WRITE(12,*)
-      WRITE(12,*) debugSTAT, ' Time Elapsed : ', REAL(COUNT_1 - COUNT_0)/ REAL(COUNT_RATE)
+      WRITE(12,*) debugSTAT, ' ELAPSED TIME : ', REAL(COUNT_1 - COUNT_0)/ REAL(COUNT_RATE)
       WRITE(12,*)
       WRITE(12,*) 'END OF PROGRAM. '
       CLOSE(12)
